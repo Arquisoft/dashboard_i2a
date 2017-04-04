ed41c6816df7e66db08aff1aff3eba65333b888e
alexbrks <alex_brks@hotmail.com>
2017-04-04 18:29:06 +0200

    pom fix2

--- src/main/java/dashboard/producers/KafkaProducer.java
+++ src/main/java/dashboard/producers/KafkaProducer.java
@@ -2,6 +2,8 @@ package dashboard.producers;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
