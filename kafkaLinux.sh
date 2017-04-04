bfca0894039ad8b0f43eb939bad130dba513e4f9
alexbrks <alex_brks@hotmail.com>
2017-04-04 23:40:08 +0200

    travis test 2

--- kafkaLinux.sh
+++ kafkaLinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/binaries/zookeeper-server-start.sh /config/zookeeper.properties;
+./binaries/zookeeper-server-start.sh /config/zookeeper.properties;
 
 echo "Waiting for zookeeper to start";
 sleep 7
 
-/binaries/kafka-server-start.bat /config/server.properties;
+./binaries/kafka-server-start.bat /config/server.properties;
