56c57d357949ad41567bfc982c99c3634355e714
Daniel <sflock1@gmail.com>
2017-04-04 19:51:02 +0200

    Fixed .gitIgnore issue

--- kafkaWindows.bat
+++ kafkaWindows.bat
@@ -5,9 +5,9 @@ cd %TEMP%\kafka
 echo Waiting for robocopy to end cloning kafka
 TIMEOUT 7
 
-start bin\windows\zookeeper-server-start.bat config\zookeeper.properties
+start binaries\windows\zookeeper-server-start.bat config\zookeeper.properties
 
 echo Waiting for zookeeper to start
 TIMEOUT 7
 
-bin\windows\kafka-server-start.bat config\server.properties
+binaries\windows\kafka-server-start.bat config\server.properties
